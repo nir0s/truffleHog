bbb83bfaa8c462923a666f5af51fcc2be40e5836
nir0s <nir36g@gmail.com>
2018-01-02 14:49:22 +0200

    Cleanup handler

--- faaspot/handler.py
+++ faaspot/handler.py
@@ -1,25 +1,12 @@
-# https://pypi.python.org/pypi/tmdbv3api
-
-# Implement your function here.
-# The function will get the event as the first parameter with query/body properties:
-# The function should return a Dictionary
-import os
 import json
-import urlparse
 
 from truffleHog import truffleHog as th
-# from box import Box
 
 
 def hog(event, context):
     params = event.get("query", {})
     outcome = th.find_strings(params['git_url'])
     return _build_response(outcome)
-
-
-def manual_hog(git_url):
-    outcome = th.find_strings(git_url, printJson=True)
-    print(outcome)
 
 
 def _build_response(response, stringify=False):
@@ -29,7 +16,3 @@ def _build_response(response, stringify=False):
         'body': response,
         'headers': {"Content-Type": "application/json"}
     }
-
-
-if __name__ == '__main__':
-    manual_hog('https://github.com/seekrets/seekrets.git')
